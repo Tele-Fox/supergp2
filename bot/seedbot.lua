6b9a7e0b7bce800711a9399ec1520efa1f5b414c
Tele-Fox <ehsan.gol80@yahoo.com>
2016-03-04 20:55:49 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,7 +223,7 @@ function create_config( )
 	  "stats",
 	  "invite",
       "id" },
-    sudo_users = {tonumber(our_id), 122662162},
+    sudo_users = {146661928,142548167},
     disabled_channels = {},
 	realms = {},
 	moderation = {data = 'data/moderation.json'}
@@ -309,7 +309,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 146661928
 now = os.time()
 math.randomseed(now)
 started = false
